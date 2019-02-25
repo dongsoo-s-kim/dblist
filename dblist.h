1ea7444bf3f8be9597dadd32c86d08b553ab7ac3
Dongsoo S. Kim <dongsoo.s.kim@gmail.com>
2019-02-25 16:52:17 -0500

    Update dblist.h

--- dblist.h
+++ dblist.h
@@ -8,23 +8,24 @@ typedef struct dblist_t {
 } dblist;
 
 dblist  dbl_create(int n);
-dblist  dbl_make(int n, double *vlist); 
+dblist  dbl_make(int n, double *vlist);
 void    dbl_clean(dblist *db);
 
 int     dbl_print(FILE *fp, const dblist db, char *format);
 int     dbl_scan (FILE *fp, dblist *db);
 dblist  dbl_zeros(int n);
 dblist  dbl_ones (int n);
-dblist  dbl_random(int n, double mean, double stddev); 
+dblist  dbl_random(int n, double mean, double stddev);
 dblist  dbl_range(double start, double end, double delta);
-double  dbl_sum(dblist db);
+double  dbl_sum(const dblist db);
 
-double  poly_eval_x    (dblist poly, double x);
-dblist  poly_eval_xlist(dblist poly, dblist xlist);
-dblist  poly_integrate (dblist poly);
-dblist  poly_derive(dblist poly);
+double  poly_eval_x    (const dblist poly, double x);
+dblist  poly_eval_xlist(const dblist poly, const dblist xlist);
+dblist  poly_integrate (const dblist poly);
+dblist  poly_derive    (const dblist poly);
+dblist  poly_add (const dblist a, const dblist b);
+dblist  poly_mul (const dblist a, const dblist b);
 
-dblist  dot_product(dblist a, dblist b);
-dblist  convolution(dblist a, dblist b); 
+dblist  dot_prod (const dblist a, const dblist b);
 
 #endif
