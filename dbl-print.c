c87b1b8fe32fb1a0daa41833006c485b4f65c0ef
Dongsoo S. Kim <dongsoo.s.kim@gmail.com>
2019-02-25 16:35:52 -0500

    Add files via upload

---
+++ dbl-print.c
@@ -0,0 +1,12 @@
+#include "dblist.h"
+#include <stdio.h>
+
+int     dbl_print(FILE *fp, const dblist db, char *format)
+{
+  int cnt = 0; 
+  for (int i=0; i<db.num; i++) {
+    cnt += fprintf(fp, format, db.val[i]);
+    cnt += fprintf(fp, "%s", (i<db.num-1) ? ", " : "\n");
+  }
+  return cnt; 
+}
