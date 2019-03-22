2946d9e166bb8e23c0c86318fc76b89144e15809
dongsoo-s-kim <dongsoo.s.kim@gmail.com>
2019-03-22 12:09:55 -0400

    new

---
+++ dbl-alloc.c
@@ -0,0 +1,30 @@
+/*
+  dbl-alloc.c
+  Copyright (C) 2019 Dongsoo S. Kim
+ */
+#include "dblist.h"
+#include <stdlib.h>
+
+dblist  dbl_create(int n)
+{
+  dblist x;
+  x.num = n;
+  x.val = (double *) malloc(sizeof(double) * x.num);
+  return x;
+}
+
+dblist dbl_make(int n, double *vlist)
+{
+  dblist x = dbl_create(n);
+  for (int i=0; i<x.num; i++)
+    x.val[i] = vlist[i];
+  return x;
+}
+
+
+void    dbl_clean(dblist *db)
+{
+  free(db->val);
+  db->val = NULL;
+  db->num = 0;
+}
